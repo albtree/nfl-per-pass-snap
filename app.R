8f7911094bf6c198d282196b6163054f266345a4
Tim <88771954+albtree@users.noreply.github.com>
2023-05-18 22:55:06 +1000

    Update app.R

--- app.R
+++ app.R
@@ -70,7 +70,7 @@ output$epapps_plot <- renderPlot({if (is.null(filtered())) {return()}
     labs(title = "Targets & Receiving EPA per Pass Snap",
          x = "Targets per Pass Snap",
          y = "Receiving EPA per Pass Snap",
-         caption = "Data = nflverse. Author = @TAlbTree")+xlim(0.1,0.4)+ylim(-0.25,0.35)+
+         caption = "Data = nflverse. Author = @TAlbTree Link: https://albtree.shinyapps.io/routes_shiny/")+xlim(0.1,0.4)+ylim(-0.25,0.35)+
     theme_ipsum_rc()+
     theme(plot.title = element_text(size = 12),
           plot.subtitle = element_text(size = 12),
