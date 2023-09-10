322efb35c65fd142bcb88f492e956fbfcdbbd04d
Tim <88771954+albtree@users.noreply.github.com>
2023-09-10 20:33:30 +1000

    Update snaps_nflverse.R

--- R/snaps_nflverse.R
+++ R/snaps_nflverse.R
@@ -2,14 +2,14 @@ library(tidyverse)
 library(nflverse)
 
 ## Once NFL season has started, change 2022 to 2023
-participation <- load_participation(2022) %>%
+participation <- load_participation(2023) %>%
   mutate(possession_team = case_when(possession_team == "OAK" ~ "LV",
                         TRUE ~ possession_team))
 
 df2 <- participation %>%
   separate_rows(offense_players, sep = ";") ## Oakland missing offense player IDs - have logged github issue
 ## Once NFL season has started, change 2022 to 2023
-rosters <- load_rosters(2022) %>%
+rosters <- load_rosters(2023) %>%
   select(position, full_name, gsis_id, team, season, years_exp) %>%
   filter(position == "WR" | position == "TE" | position == "RB")
 
@@ -17,7 +17,7 @@ rosters2 <- rosters %>%
   select(position, gsis_id, team, season) %>%
   distinct(gsis_id, team, season, .keep_all = TRUE)
 ## Once NFL season has started, change 2022 to 2023
-pbp <- load_pbp(2022)
+pbp <- load_pbp(2023)
 
 df3 <- pbp %>%
   left_join(df2, by = c('old_game_id' = 'old_game_id', 'play_id' = 'play_id'), na_matches = "never") %>%
@@ -25,7 +25,7 @@ df3 <- pbp %>%
   select(season, posteam, desc, air_yards, yards_after_catch, epa, receiver_player_id, receiver_player_name, offense_players) %>%
   left_join(rosters, by = c('offense_players' = 'gsis_id', 'season' = 'season'), na_matches = "never")
 ## Once NFL season has started, change 2022 to 2023
-stats <- load_player_stats(2022)%>%
+stats <- load_player_stats(2023)%>%
   filter(week <= 18) %>%
   group_by(player_id, season, recent_team) %>%
   summarise(rec_yards_total = sum(receiving_yards),
@@ -52,8 +52,8 @@ routes <- df3 %>%
   left_join(rosters2, by = c('offense_players' = 'gsis_id', 'season' = 'season'), na_matches = "never") 
 
 ## Once 2023 NFL season has started, change this to 2016_2022
-routes_2016_2021 <- read.csv("read_data/routes_2016_2021.csv")
+routes_2016_2022 <- read.csv("read_data/routes_2016_2022.csv")
 ## change routes_2016_2021 to 2016_2022
-routes_all <- bind_rows(routes, routes_2016_2021)
+routes_all <- bind_rows(routes, routes_2016_2022)
 
 write.csv(routes_all, file = "rec_per_snap_data.csv", row.names = F)
